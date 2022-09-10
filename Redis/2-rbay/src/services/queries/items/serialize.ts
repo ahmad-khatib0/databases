73891d738b4199e8b2bbe4a9eb13005abd9ab027
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-09-10 17:54:33 +0300

    get one, get all, create one. for users, items, sessions, with redis

--- Redis/2-rbay/src/services/queries/items/serialize.ts
+++ Redis/2-rbay/src/services/queries/items/serialize.ts
@@ -1,3 +1,5 @@
 import type { CreateItemAttrs } from '$services/types';
 
-export const serialize = (attrs: CreateItemAttrs) => {};
+export const serialize = (attrs: CreateItemAttrs) => {
+	return { ...attrs, createdAt: attrs.createdAt.toMillis(), endingAt: attrs.endingAt.toMillis() };
+};
