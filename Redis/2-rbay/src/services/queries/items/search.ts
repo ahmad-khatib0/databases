62ef23aea7a40a216a87bbc6ae81c01149c06f8b
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-09-17 18:28:10 +0300

    give the prioirty to the name field when searching over the description, using the

--- Redis/2-rbay/src/services/queries/items/search.ts
+++ Redis/2-rbay/src/services/queries/items/search.ts
@@ -1,1 +1,22 @@
-export const searchItems = async (term: string, size: number = 5) => {};
+import { client } from '$services/redis';
+import { deserialize } from './deserialize';
+import { itemsIndexKey } from '$services/keys';
+
+export const searchItems = async (term: string, size: number = 5) => {
+	//not alpha numeric, or spaces
+	console.log(term);
+
+	const cleaned = term
+		.replace(/[^a-zA-Z0-9 ]/g, '')
+		.trim()
+		.split(' ')
+		.map((word) => (word ? `%${word}%` : ''))
+		.join(' '); // ' ' means AND ,, or \ for OR
+
+	const query = `(@name:(${cleaned}) => { $weight: 5.0 }) | (@description:(${cleaned}))`;
+	if (cleaned == '') return [];
+
+	const results = await client.ft.search(itemsIndexKey(), query, { LIMIT: { from: 0, size } });
+	return results.documents.map(({ id, value }) => deserialize(id, value as any));
+	// console.log(result);
+};
