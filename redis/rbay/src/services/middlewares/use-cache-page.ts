d9865dc6eac67344458b721daca26a9c1f5db7cd
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-06-17 15:49:45 +0300

    connect to pg with node js

--- redis/rbay/src/services/middlewares/use-cache-page.ts
+++ redis/rbay/src/services/middlewares/use-cache-page.ts
@@ -19,6 +19,10 @@ export const useCachePage: Handle = async ({ event, resolve }) => {
 		});
 	}
 
+	// if-none-match This header is commonly used in caching scenarios, where a client can store 
+	// a copy of a resource and use the If-None-Match header to check if the resource has been 
+	// modified on the server. If the resource has not been modified, the client can use the cached 
+	// copy instead of requesting the resource again.
 	event.request.headers.set('if-none-match', Math.random().toString());
 	const res = await resolve(event);
 
