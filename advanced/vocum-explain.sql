94f1d93f7aba04b15fd3801a6fb885683212e1c0
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2024-04-21 16:43:24 +0300

    rename

--- advanced/vocum-explain.sql
+++ advanced/vocum-explain.sql
@@ -3,21 +3,17 @@
 -- make sure to run the container with at least 1gb shared memory
 -- docker run --name pg —shm-size=1g -e POSTGRES_PASSWORD=postgres —name pg postgres
 
-
-
 create table grades (
-id serial primary key, 
- g int,
- name text 
+   id serial primary key, 
+   g int,
+   name text 
 ); 
 
-
-insert into grades (g,
-name  ) 
-select 
-random()*100,
-substring(md5(random()::text ),0,floor(random()*31)::int)
- from generate_series(0, 500);
+INSERT INTO grades (g, name) 
+SELECT 
+    random()*100,
+    substring(md5(random()::text ),0,floor(random()*31)::int)
+FROM generate_series(0, 500);
 
 vacuum (analyze, verbose, full);
 
